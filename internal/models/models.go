bb4533c1ab2568eeb90f9e32c468737697f73f9b
Doston <dostonlv@icloud.com>
2024-03-16 10:41:25 +0500

    Order insert add

--- internal/models/models.go
+++ internal/models/models.go
@@ -37,6 +37,7 @@ type Order struct {
 	TransactionID int       `json:"transaction_id"`
 	StatusID      int       `json:"status_id"`
 	Quantity      int       `json:"quantity"`
+	Amount        int       `json:"amount"`
 	CreatedAt     time.Time `json:"-"`
 	UpdatedAt     time.Time `json:"-"`
 }
@@ -122,6 +123,37 @@ func (m *DBModel) InsertTransaction(txn Transaction) (int, error) {
 		txn.LastFour,
 		txn.BankReturnCode,
 		txn.TransactionStatusID,
+		time.Now(),
+		time.Now())
+
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return int(id), nil
+}
+
+func (m *DBModel) InsertOrder(order Order) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `
+	   insert into orders
+	    	(widget_id, transaction_id, status_id,quantity,
+				amount,created_at,updated_at)
+		values(?,?,?,?,?,?,?)
+	`
+
+	result, err := m.DB.ExecContext(ctx, stmt,
+		order.WidgetID,
+		order.TransactionID,
+		order.StatusID,
+		order.Quantity,
+		order.Amount,
 		time.Now(),
 		time.Now())
 
